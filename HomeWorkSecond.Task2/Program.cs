a8ea19cf8480a2584b1f6feecfd0e57bbd8e561d
Rostislav <lavrenovrostislav@gmail.com>
2022-02-24 18:36:04 +0300

    Completed Task2

--- HomeWorkSecond.Task2/Program.cs
+++ HomeWorkSecond.Task2/Program.cs
@@ -1,7 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координату Х:");
+int coordinateX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y:");
+int coordinateY = Convert.ToInt32(Console.ReadLine());
+
+if (coordinateX > 0 && coordinateY > 0)
+{
+    Console.WriteLine("Точка принадлежит I четверти");
+}
+else if (coordinateX < 0 && coordinateY > 0)
+{
+    Console.WriteLine("Точка принадлежит II четверти");
+}
+else if (coordinateX <0 && coordinateY < 0)
+{
+    Console.WriteLine("Точка принадлежит III четверти");
+}
+else
+{
+    Console.WriteLine("Точка принадлежит IV четверти");
+}
