c2a67357dc4d3d0cbc555c9bbd1f00c9f30b00cd
Rostislav <lavrenovrostislav@gmail.com>
2022-02-24 23:17:55 +0300

    Started work on Task5

---
+++ HomeWorkSecond.Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int twoDigitNumber=Convert.ToInt32(Console.ReadLine());
+if (twoDigitNumber == 10)
+{
+    Console.WriteLine("Десять");
+}
+else if (twoDigitNumber == 11)
+{
+    Console.WriteLine("Одиннадцать");
+}
+else if (twoDigitNumber == 12)
+{
+    Console.WriteLine("Двеннадцать");
+}
+else if (twoDigitNumber == 13)
+{
+    Console.WriteLine("Триннадцать");
+}
